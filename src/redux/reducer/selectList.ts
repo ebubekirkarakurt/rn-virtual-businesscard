0140592fc48ae814dba8b63a342e13d648c734b5
ebubekirkarakurt <ebukar75@gmail.com>
2025-02-16 20:30:52 +0300

    feat: userId info added into reducer

--- src/redux/reducer/selectList.ts
+++ src/redux/reducer/selectList.ts
@@ -1,23 +1,40 @@
-import {PayloadAction, createSlice} from '@reduxjs/toolkit';
+import { PayloadAction, createSlice } from "@reduxjs/toolkit";
+
+type UserType = {
+  id: string;
+  name: string;
+  surName: string;
+  phoneNumber: string;
+  mail: string;
+  address: string;
+  instagram: string;
+  website: string;
+};
 
 type ListType = {
-  myList: string[];
+  myList: UserType[];
+  userCount: number;
 };
 
 const initialState: ListType = {
   myList: [],
+  userCount: 0,
 };
 
 export const selectList = createSlice({
   name: 'setList',
   initialState,
   reducers: {
-    addItem: (state, action: PayloadAction<string>) => {
-      state.myList.push(action.payload);
+    addItem: (state, action: PayloadAction<Omit<UserType, 'id'>>) => {
+      state.userCount += 1; 
+      const newUser = {
+        id: `user${state.userCount}`, // user1, user2, user3
+        ...action.payload,
+      };
+      state.myList.push(newUser);
     },
   },
 });
 
-export const {addItem} = selectList.actions;
-
+export const { addItem } = selectList.actions;
 export default selectList.reducer;
