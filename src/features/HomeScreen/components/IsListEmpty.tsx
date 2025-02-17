c9fd9adfd03b9c0abf06102b986dd30210b028a2
ebubekirkarakurt <ebukar75@gmail.com>
2025-02-17 23:33:20 +0300

    feat: IsListEmpty created

---
+++ src/features/HomeScreen/components/IsListEmpty.tsx
@@ -0,0 +1,16 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+type Props = {}
+
+const IsListEmpty = (props: Props) => {
+  return (
+    <View>
+      <Text>IsListEmpty</Text>
+    </View>
+  )
+}
+
+export default IsListEmpty;
+
+const styles = StyleSheet.create({})
